b7426cbc59f94798275fd754cedbf946b2552282
Prafull Bhosale <prbhosal@microsoft.com>
2016-09-07 16:28:57 -0700

    Add MsBuildProjectContext and RoslynWorkspace

---
+++ src/Microsoft.VisualStudio.Web.CodeGeneration.MsBuild/LibraryManager.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.Web.CodeGeneration.MsBuild
+{
+    public class LibraryManager
+    {
+        private string _depsFile;
+        private DependencyContext _dependencyContext = null;
+        public LibraryManager(MsBuildProjectContext msBuildProjectContext)
+        {
+            if (msBuildProjectContext == null)
+            {
+                throw new ArgumentNullException(nameof(msBuildProjectContext));
+            }
+
+            _depsFile = msBuildProjectContext.DepsJson;
+            Init();
+        }
+
+        private void Init()
+        {
+            if (File.Exists(_depsFile))
+            {
+                using (var stream = File.OpenRead(_depsFile))
+                {
+                    _dependencyContext = new DependencyContextJsonReader().Read(stream);
+                }
+            }
+        }
+
+        public Library GetLibrary(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return _dependencyContext?.CompileLibraries?.FirstOrDefault(l => l.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IEnumerable<Library> GetLibraries()
+        {
+
+            return _dependencyContext?.CompileLibraries;
+        }
+
+        public IEnumerable<Library> GetReferencingLibraries(string name)
+        {
+            return _dependencyContext.CompileLibraries.Where(l => HasDependency(l, name));
+        }
+
+        private bool HasDependency(CompilationLibrary l, string name)
+        {
+            return l.Dependencies.Any(d => d.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+    }
+}
---
+++ src/Microsoft.VisualStudio.Web.CodeGeneration.MsBuild/MsBuildProjectContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.Web.CodeGeneration.MsBuild
+{
+    public static class MsBuildProjectContextExtensions
+    {
+        public static LibraryManager CreateLibraryManager(this MsBuildProjectContext context)
+        {
+            return new LibraryManager(context);
+        }
+
+        public static RoslynWorkspace CreateRoslynWorkspace(this MsBuildProjectContext context, string configuration = "debug")
+        {
+            return new RoslynWorkspace(context, configuration);
+        }
+    }
+}
